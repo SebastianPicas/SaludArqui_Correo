6a74dfb560530afb31c9746d92bc2d70734a13a7
sebastianpica <sebas.picas08@gmail.com>
2024-11-20 14:48:13 -0500

    Conecta a Rabbit

--- src/main/java/com/saludarqui/correos/listener/EventListener.java
+++ src/main/java/com/saludarqui/correos/listener/EventListener.java
@@ -1,9 +1,11 @@
 package com.saludarqui.correos.listener;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventListener {
 
+    @RabbitListener
     public void notificationListener() {
 
     }
