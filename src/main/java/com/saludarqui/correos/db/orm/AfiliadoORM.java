6db744fa8bb204cfd7fbf00690062758d1b3c4d8
sebastianpica <sebas.picas08@gmail.com>
2024-11-20 15:11:18 -0500

    Conecta a la base de datos

---
+++ src/main/java/com/saludarqui/correos/db/orm/AfiliadoORM.java
@@ -0,0 +1,33 @@
+package com.saludarqui.correos.db.orm;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Table(name = "afiliado")
+@Entity
+@Data
+@NoArgsConstructor
+public class AfiliadoORM {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_afiliado")
+    private Long idAfiliado;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private Integer edad;
+
+    @Column
+    private String email;
+
+    @Column
+    private String genero;
+
+}
---
+++ src/main/java/com/saludarqui/correos/db/orm/BeneficiarioORM.java
@@ -0,0 +1,31 @@
+package com.saludarqui.correos.db.orm;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Table(name = "beneficiario")
+@Entity
+@Data
+@NoArgsConstructor
+public class BeneficiarioORM {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_beneficiario")
+    private Long idBeneficiario;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private String email;
+
+    @JoinColumn(name = "id_afiliado", unique = true)
+    @OneToOne(cascade = CascadeType.ALL)
+    private AfiliadoORM afliliadoORM;
+
+}
